f97e2830e9bfcf081d8d7352be9b560ab559d612
Dev Prit <dev.prit@lcinocm.com>
2022-11-29 14:37:28 +0700

    Update params set App

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // const Path = require('path');
 
 module.exports = {
-  register({ app }, rootConfig){
-    global.App = {rootConfig};
+  register({rootConfig}){
+    global.App = global.App || {rootConfig};
     global.App.config = require('./reader/config');
     global.App.middleware = () => require('./reader/middleware');
     global.App.helpers = () => require('./reader/helper');
